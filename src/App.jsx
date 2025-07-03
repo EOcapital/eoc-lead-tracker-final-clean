c013edbefe186087a324d9cc3c34b0a714b0f667
EOcapital <oron@eocapitalgroup.com>
2025-07-02 20:47:56 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ fontFamily: "sans-serif", textAlign: "center", marginTop: "2rem" }}>
+      <h1>EOC Lead Tracker</h1>
+      <p>Welcome to your custom lead tracking app!</p>
+    </div>
+  );
+}
+
+export default App;
