47844e746a01a53d954509e16850482a6d63e4b4
EOcapital <oron@eocapitalgroup.com>
2025-07-02 22:25:16 -0400

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import LeadForm from "./LeadForm";
+
+function App() {
+  return (
+    <div style={{ fontFamily: "sans-serif", padding: "2rem" }}>
+      <h1 style={{ textAlign: "center" }}>EOC Lead Tracker</h1>
+      <LeadForm />
+    </div>
+  );
+}
+
+export default App;
